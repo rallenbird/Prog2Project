b6e48d5a7b9a7830a1ec9e051c86c99bf0e84cdb
rallenbird <ralle_strangeman@yahoo.de>
2015-05-16 10:30:06 +0200

    init commit.

---
+++ src/prog2project/Prog2Project.java
@@ -0,0 +1,17 @@
+
+package prog2project;
+
+/**
+ *
+ * @author Valentin, Dodo, Lukas, Minx
+ */
+public class Prog2Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
